73d1bd0fed0f26426bf3f569c39975abd88c1f6f
0andreea <andreeamarginas@gmail.com>
2024-07-30 22:36:41 +0300

    first commit with base project

---
+++ src/main/java/com/TravelChronicles/TravelChronicles/TravelChroniclesApplication.java
@@ -0,0 +1,13 @@
+package com.TravelChronicles.TravelChronicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelChroniclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelChroniclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TravelChronicles/TravelChronicles/TravelChroniclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TravelChronicles.TravelChronicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelChroniclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
